bbb0c31a9ea3120c7dd0b3a8cf4b709520c974af
jonaslunafreitas <jonasluna.cont@gmail.com>
2022-11-27 06:14:09 -0300

    Responsive and animations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 var swiper = new Swiper(".slide-characterts", {
     slidesPerView: 3.5,
     spaceBetween: 19,
-    freeMode: true
+    freeMode: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1.1,
+
+        },
+        768: {
+            slidesPerView: 2.2,
+
+        },
+        991: {
+            slidesPerView: 2.8,
+
+        },
+        991: {
+            slidesPerView: 3.5,
+
+        },
+    },
+});
+
+AOS.init({
+    duration: 1000,
+    once: true,
 });
